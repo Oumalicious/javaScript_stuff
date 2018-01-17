4a39dae8a453dea4ec59ec864bd7b0714403dc71
vincent xue <whydoesntsenpainotice@gmail.com>
2018-01-17 10:31:03 -0500

    commented out example array

--- allStar/StringEditor.js
+++ allStar/StringEditor.js
@@ -5,10 +5,10 @@ class StringEditor extends Component {
 	constructor(props){
 		super(props);
 		var img_array = ['/images/blue_heart_32.png','/images/green_heart_32.png','/images/yellow_heart_32.png','/images/olive_heart_32.png'];
-		var options = [
-			{ label : "George", value : "0001" },
-			{ label : "Mary", value : "9999" }
-		];
+//		var options = [
+//			{ label : "George", value : "0001" },
+//			{ label : "Mary", value : "9999" }
+//		];
 		this.state = {
 			example:'Try to edit this String.',
 			editField:'',
@@ -19,7 +19,8 @@ class StringEditor extends Component {
 			imgSRC: img_array[0],
 			timer:'Hover this text to start a timer.',
 			timerTrigger: false,
-			options: options,
+//			optionsTemp: options,
+			options: null,
 			selectedOption:'0001',
 			optionsString:'\'nothing\'',	
 		};
@@ -59,7 +60,7 @@ class StringEditor extends Component {
 	}
 	
 	onTimerEvent(e){
-		if(this.state.timerTrigger!=true){
+		if(this.state.timerTrigger!==true){
 			var that = this;
 			that.setState({timerTrigger: true});
 			var startingTime = new Date().getTime();
@@ -84,17 +85,17 @@ class StringEditor extends Component {
 						<input type='button' onClick={this.onEditEvent.bind(this)} value={this.state.EditButton}/>
 						<input type='button' onClick={this.onClearEvent.bind(this)} value={this.state.ClearButton}/>
 						<br /> <br />
-						<img src={this.state.imgSRC} onMouseOver={this.onImgClickEvent.bind(this) }/>
+						<img src={this.state.imgSRC} onMouseOver={this.onImgClickEvent.bind(this) }/> 			<br /> <br />
 						<div onMouseOver={this.onTimerEvent.bind(this)} >
 							{this.state.timer}
 						</div>
-				</div>			<br /> <br /> <br /> <br /> <br /> <br />
+				</div>			 <br />
 				<form>	
 						<FormSelect
 							options={this.state.options}
 							value={this.state.selectedOption}
 							onChange={this.onOptionChange.bind(this)}
-						/>
+						/>			<br />
 						<div>
 							You selected {this.state.optionsString}.
 						</div>
