43e482079bd0fb56966abcded1ec6537bc4b8f97
vincent xue <whydoesntsenpainotice@gmail.com>
2018-02-21 14:11:18 -0500

    started paid leave grid

---
+++ allStar/PaidLeaveGrid.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import * as DataStore from './DataStore';
+import {FormSelect} from 'elemental';
+import {FormField} from 'elemental';
+import Moment from 'moment';
+import {AgGridReact} from 'ag-grid-react';
+import 'ag-grid/dist/styles/ag-grid.css';
+import {PaidLeaveEligibility} from './PaidLeaveEligibility.js';
+
+class PaidLeaveGrid extends Component {
+	constructor(props){
+		super(props);
+		var columns = [
+			{headerName: "Week", field : "week", suppressMovable : true},
+			{headerName: "Eligible", field : "eligible", suppressMovable : true},
+			{headerName: "Eligible Date", field : "eligible_date", suppressMovable : true},
+			{headerName: "Hours Met Date", field : "hours_met_date", suppressMovable : true},
+			{headerName: "Days Met", field : "days_met_date", suppressMovable : true},
+			{headerName: "Total Hours", field : "total_hours", suppressMovable : true},
+			{headerName: "Days Employed", field : "days_employed", suppressMovable : true},
+		];	
+		this.state = {
+			columnDefs : columns,
+			rowData : null,
+		}
+	}
+	componentWillMount(){
+		var temp_array = PaidLeaveEligibility(20170716, GetSamplePayrolls());
+		for(var index in temp_array){
+			temp_array.push({
+				week: temp_array[index].week,
+				eligible: temp_array[index].eligible,
+				eligible_date: temp_array[index].eligible_date,
+				hours_met_date: temp_array[index].hours_met,
+				days_met_date: temp_array[index].days_met,
+				total_hours: temp_array[index].total_hours,
+				days_employed: temp_array[index].days_employed,
+			});
+		}
+		this.setState({rowData : temp_array});
+	}
+	render(){
+		var divContainer = {marginTop: '0', width: '100%', height: '100%'};
+		var grid_style={width: '100%', height: '500px'};
+		return (
+			<div style={divContainer} className='ag-blue'>
+				<div style={grid_style}>
+					<AgGridReact
+						columnDefs={this.state.columnDefs}
+						rowData={this.state.rowData}
+						enableSorting
+						enableFilter
+					/>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default PaidLeaveGrid
